7af83efbeda5cf95fc06d1fb428e1aca7e4f7911
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-09-10 19:20:54 +0200

    LUNA VIP ADMIN completo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dh-bg': '#0a0a0a',
+        'dh-card': '#111111',
+        'dh-border': '#1f1f1f',
+        'dh-green': '#00ff88',
+      },
+    },
+  },
+  plugins: [],
+};
